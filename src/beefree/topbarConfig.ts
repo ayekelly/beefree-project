57c887bafda26f9ae3308e79ed43fddfb26fcdbe
Alex Wilson <alex_kelly@intuit.com>
2026-06-08 19:59:34 -0400

    remove unused actions

--- src/beefree/topbarConfig.ts
+++ src/beefree/topbarConfig.ts
@@ -3,9 +3,7 @@ import type { IBeeConfig } from '@beefree.io/react-email-builder'
 export const topbarTranslations: NonNullable<IBeeConfig['translations']> = {
   'bee-common-top-bar': {
     actions: 'Actions',
-    'send-test': 'Send test',
     save: 'Save',
-    'save-as-template': 'Save as a template',
     preview: 'Preview',
     'show-structure': 'Show structure',
     help: 'Help',
